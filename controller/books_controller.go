2d0784e02814d2ed780aaad6002140f44ea21fb5
rezanur <rezanur@uii.ac.id>
2019-01-02 14:29:01 +0700

    finish example crud in go

--- controller/books_controller.go
+++ controller/books_controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"Golang-API-Testing/config"
 	"Golang-API-Testing/model"
+	"github.com/satori/go.uuid"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -15,14 +16,14 @@ var initializeDatabaseConnection = config.DatabaseConn()
 func GetAllBooks(c *gin.Context) {
 
 	var (
-		books      []model.BooksModel
+		books      []model.Books
 		countBooks int
 	)
 
-	initializeDatabaseConnection.Table("book").
-		Select("book.uuid_books, book.book_name, book.book_writer, book.book_publisher," +
-			"book.book_description, book_category.book_category_name").
-		Joins("INNER JOIN book_category ON book_category.uuid_book_category=book.uuid_book_category").
+	initializeDatabaseConnection.Table("books").
+		Select("books.uuid_books, books.book_name, books.book_writer, books.book_publisher," +
+			"books.book_description, book_category.book_category_name").
+		Joins("INNER JOIN book_category ON book_category.uuid_book_category=books.uuid_book_category").
 		Find(&books).Count(&countBooks)
 
 	c.JSON(http.StatusOK, gin.H{
@@ -37,15 +38,15 @@ func GetDetailBooks(c *gin.Context) {
 
 	UUIDBooks := c.Param("UUIDBooks")
 
-	var books model.BooksModel
+	var books model.Books
 
-	if initializeDatabaseConnection.Table("book").Where("book.uuid_books = ?", UUIDBooks).Find(&books).RecordNotFound() {
+	if initializeDatabaseConnection.Table("books").Where("books.uuid_books = ?", UUIDBooks).Find(&books).RecordNotFound() {
 		c.JSON(http.StatusOK, gin.H{"message": "Not found"})
 	} else {
 		initializeDatabaseConnection.Table("book").
-			Select("book.uuid_books, book.book_name, book.book_writer, book.book_publisher," +
-				"book.book_description, book_category.book_category_name").
-			Joins("INNER JOIN book_category ON book_category.uuid_book_category=book.uuid_book_category").
+			Select("books.uuid_books, books.book_name, books.book_writer, books.book_publisher," +
+				"books.book_description, book_category.book_category_name").
+			Joins("INNER JOIN book_category ON book_category.uuid_book_category=books.uuid_book_category").
 			Find(&books)
 
 		c.JSON(http.StatusOK, gin.H{"data": books})
@@ -54,17 +55,77 @@ func GetDetailBooks(c *gin.Context) {
 
 // CreateNewProducts func does create new record product
 func CreateNewBooks(c *gin.Context) {
+
+	UUIDBooks := uuid.Must(uuid.NewV4())
+	BookName := c.Param("BookName")
+	BookWriter := c.Param("BookWriter")
+	BookPublisher := c.Param("BookPublisher")
+	BookDescription := c.Param("BookDescription")
+	UUIDBookCategory := c.Param("UUIDBookCategory")
+
+	createBookPayload := model.Book{
+		UUIDBooks:        UUIDBooks,
+		BookName:         BookName,
+		BookWriter:       BookWriter,
+		BookPublisher:    BookPublisher,
+		BookDescription:  BookDescription,
+		UUIDBookCategory: UUIDBookCategory,
+	}
+
+	c.BindJSON(&createBookPayload)
+
+	initializeDatabaseConnection.Create(createBookPayload)
+
 	c.JSON(http.StatusOK, gin.H{"message": "Inserted sucessfully"})
 }
 
 // UpdateProducts func does update record product
 func UpdateBooks(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Updated successfully"})
+
+	var booksModel model.Book
+
+	UUIDBooks := c.Param("UUIDBooks")
+	BookName := c.Param("BookName")
+	BookWriter := c.Param("BookWriter")
+	BookPublisher := c.Param("BookPublisher")
+	BookDescription := c.Param("BookDescription")
+	UUIDBookCategory := c.Param("UUIDBookCategory")
+
+	updateBookPayload := model.Book{
+		BookName:         BookName,
+		BookWriter:       BookWriter,
+		BookPublisher:    BookPublisher,
+		BookDescription:  BookDescription,
+		UUIDBookCategory: UUIDBookCategory,
+	}
+
+	c.BindJSON(&updateBookPayload)
+
+	if initializeDatabaseConnection.Where("books.uuid_books = ?", UUIDBooks).Find(&booksModel).RecordNotFound() {
+		c.JSON(http.StatusOK, gin.H{"message": "Data not found"})
+	} else {
+		initializeDatabaseConnection.Where("books.uuid_books = ?", UUIDBooks).
+			Table("books").Update(&updateBookPayload)
+
+		c.JSON(http.StatusOK, gin.H{"message": "Updated successfully"})
+	}
 }
 
 // DeleteProducts func does delete record product
 func DeleteBooks(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Deleted successfully"})
+
+	var booksModel model.Book
+
+	UUIDBooks := c.Param("UUIDBooks")
+
+	if initializeDatabaseConnection.Where("books.uuid_books = ?", UUIDBooks).Find(&booksModel).RecordNotFound() {
+		c.JSON(http.StatusOK, gin.H{"message": "Data not found"})
+	} else {
+		initializeDatabaseConnection.Where("books.uuid_books = ?", UUIDBooks).
+			Find(&booksModel).Delete(&booksModel)
+
+		c.JSON(http.StatusOK, gin.H{"message": "Deleted successfully"})
+	}
 }
 
 func ExampleTesting(c *gin.Context) {
--- model/books_model.go
+++ model/books_model.go
@@ -3,11 +3,20 @@ package model
 import "github.com/satori/go.uuid"
 
 // BooksModel struct
-type BooksModel struct {
+type Books struct {
 	UUIDBooks        uuid.UUID
 	BookName         string
 	BookWriter       string
 	BookPublisher    string
 	BookDescription  string
 	BookCategoryName string
+}
+
+type Book struct {
+	UUIDBooks        uuid.UUID
+	BookName         string
+	BookWriter       string
+	BookPublisher    string
+	BookDescription  string
+	UUIDBookCategory string
 }
